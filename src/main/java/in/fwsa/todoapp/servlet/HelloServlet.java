f482ea4877fe4db982e2a99ebd42a264ec819402
csys-batch-08 <96282618+csys-batch-08@users.noreply.github.com>
2022-01-21 06:02:57 +0530

    Initial commit

---
+++ src/main/java/in/fwsa/todoapp/servlet/HelloServlet.java
@@ -0,0 +1,42 @@
+package in.fwsa.todoapp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloServlet123 - GET Request");
+		response.getWriter().append("HelloServlet - GET Request");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println("HelloServlet - POST Request");
+		response.getWriter().append("HelloServlet - POST Request");
+	}
+
+}
---
+++ src/test/java/in/fwsa/todoapp/servlet/HelloServletTest.java
@@ -0,0 +1,8 @@
+package in.fwsa.todoapp.servlet;
+
+public class HelloServletTest {
+
+	public static void main(String[] args) {
+		System.out.println("HelloServlet Testing");
+	}
+}
